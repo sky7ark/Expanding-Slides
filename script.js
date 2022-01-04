21106de6003356932db211c0c031253bd2061c9b
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2022-01-04 18:09:30 +0600

    updated mouse over event listener

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
+  panel.addEventListener("mouseover", () => {
     removeActiveClass();
     panel.classList.add("active");
   });
